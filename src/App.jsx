3e6a84f35632e623936b07cd2745333e259a17d0
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-10-26 19:50:25 +0530

    create delivery_location page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-
+import DeliveryLocation from './pages/DeliveryLocation';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/delivery" element={<Cart />} />
       </Routes>
     </Router>
   );
